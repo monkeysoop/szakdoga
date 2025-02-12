b96bd707391fd9858f8fba96cb90ba339260751a
monkeysoop <istok.endre2@gmail.com>
2025-02-12 19:09:38 +0100

    mipmap support

--- app/core/include/Texture2D.hpp
+++ app/core/include/Texture2D.hpp
@@ -4,14 +4,22 @@
 
 class Texture2D {
 public:
-    Texture2D(GLsizei width, GLsizei height, GLenum format);
+    Texture2D(GLsizei width, GLsizei height, GLenum format, GLsizei levels=1);
     ~Texture2D();
 
     void Resize(GLsizei width, GLsizei height);
+    void ChangeLevels(GLsizei levels);
     GLuint GetTextureID();
-    void Bind(GLuint unit, GLenum access);
+    void Bind(GLuint unit, GLenum access, GLint level=0);
 
 private:
     GLuint m_texture_id;
+
+    GLsizei m_width;
+    GLsizei m_height;
+    
     GLenum m_format;
+    GLsizei m_levels;
+
+    void Init();
 };
--- app/core/src/Texture2D.cpp
+++ app/core/src/Texture2D.cpp
@@ -1,13 +1,12 @@
 #include "Texture2D.hpp"
 
-Texture2D::Texture2D(GLsizei width, GLsizei height, GLenum format) : m_format{format} {
-    glCreateTextures(GL_TEXTURE_2D, 1, &m_texture_id);
-    glTextureStorage2D(m_texture_id, 1, format, width, height);
-
-    glTextureParameteri(m_texture_id, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTextureParameteri(m_texture_id, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTextureParameteri(m_texture_id, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTextureParameteri(m_texture_id, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+Texture2D::Texture2D(GLsizei width, GLsizei height, GLenum format, GLsizei levels) : 
+    m_width{width},
+    m_height{height},
+    m_format{format}, 
+    m_levels{levels} 
+{
+    Init();
 }
 
 Texture2D::~Texture2D() {
@@ -17,19 +16,34 @@ Texture2D::~Texture2D() {
 void Texture2D::Resize(GLsizei width, GLsizei height) {
     glDeleteTextures(1, &m_texture_id);
 
-    glCreateTextures(GL_TEXTURE_2D, 1, &m_texture_id);
-    glTextureStorage2D(m_texture_id, 1, m_format, width, height);
+    m_width = width;
+    m_height = height;
 
-    glTextureParameteri(m_texture_id, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTextureParameteri(m_texture_id, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTextureParameteri(m_texture_id, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTextureParameteri(m_texture_id, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    Init();
+}
+
+void Texture2D::ChangeLevels(GLsizei levels) {
+    glDeleteTextures(1, &m_texture_id);
+
+    m_levels = levels;
+
+    Init();
 }
 
 GLuint Texture2D::GetTextureID() {
     return m_texture_id;
 }
 
-void Texture2D::Bind(GLuint unit, GLenum access) {
-    glBindImageTexture(unit, m_texture_id, 0, GL_FALSE, 0, access, m_format);
+void Texture2D::Bind(GLuint unit, GLenum access, GLint level) {
+    glBindImageTexture(unit, m_texture_id, level, GL_FALSE, 0, access, m_format);
+}
+
+void Texture2D::Init() {
+    glCreateTextures(GL_TEXTURE_2D, 1, &m_texture_id);
+    glTextureStorage2D(m_texture_id, m_levels, m_format, m_width, m_height);
+
+    glTextureParameteri(m_texture_id, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTextureParameteri(m_texture_id, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTextureParameteri(m_texture_id, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTextureParameteri(m_texture_id, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 }
