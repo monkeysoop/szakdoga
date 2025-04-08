722eca982a6153a46d95530f5bc1b312c25451b1
monkeysoop <istok.endre2@gmail.com>
2025-04-08 23:31:30 +0200

    fixed the image grid plots

--- app/tests/plot_utils.py
+++ app/tests/plot_utils.py
@@ -3,32 +3,56 @@ import numpy as np
 
 
 
-def plot_images(images, row_labels, col_labels, x_figsize, y_figsize, title):
-    fig, axes = plt.subplots(images.shape[0], images.shape[1], figsize=(x_figsize, y_figsize))
+def plot_images(images, row_labels, col_labels, figsize, x_label, y_label, title):
+    s = images[0][0].shape
 
-    fig.suptitle(title, size="large", fontweight="bold", y=1.05)
+    h = s[0]
+    w = s[1]
 
-    for i in range(images.shape[0]):
-        for j in range(images.shape[1]):
-            ax = axes[i, j]
-
-            ax.imshow(images[i, j])
-
-            #ax.axis('off')
-            ax.set_xticks([])
-            ax.set_yticks([])
-            ax.set_xticklabels([])
-            ax.set_yticklabels([])
-            ax.spines['top'].set_visible(False)
-            ax.spines['bottom'].set_visible(False)
-            ax.spines['left'].set_visible(False)
-            ax.spines['right'].set_visible(False)
+    for row in images:
+        for i in row:
+            if (i.shape != s):
+                print("all images must have the same shape")
+                return
             
-            if (i == 0):
-                ax.set_title(col_labels[j], size="large", pad=10)
-            
-            if (j == 0):
-                ax.set_ylabel(row_labels[i], size="large", rotation=0, labelpad=30, va="center", ha="right")
+    rows = images.shape[0]
+    cols = images.shape[1]
+
+    t = images[0][0].dtype
+
+    image_grid = np.zeros((rows * h, cols * w, s[2]), dtype=t) if (len(s) == 3) else np.zeros((rows * h, cols * w), dtype=t)
+
+    for y in range(rows):
+        for x in range(cols):
+            image = images[y][x]
+            y_offset = y * h
+            x_offset = x * w
+            if (len(s) == 3):
+                image_grid[y_offset:(y_offset + h), x_offset:(x_offset + w), :] = image
+            else:
+                image_grid[y_offset:(y_offset + h), x_offset:(x_offset + w)] = image
+
+    if (np.issubdtype(t, np.floating)):
+        mi = np.min(image_grid)
+        ma = np.max(image_grid)
+        image_grid = ((image_grid - mi) / (ma - mi)) * 255.0
+
+    image_grid = image_grid.astype(np.uint8)
+    
+    fig, ax = plt.subplots(figsize=(figsize, figsize * int(image_grid.shape[1] / image_grid.shape[0])))
+    
+    cax = ax.imshow(image_grid, aspect="equal")
+
+    ax.axis('off')
+    ax.set_xticks(np.arange(w // 2, len(col_labels) * w, w))
+    ax.set_xticklabels(col_labels)
+    ax.set_yticks(np.arange(h // 2, len(row_labels) * h, h))
+    ax.set_yticklabels(row_labels)
+    
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+
+    ax.set_title(title)
     
     plt.tight_layout()
     plt.show()
@@ -37,7 +61,7 @@ def plot_heatmap(matrix, row_labels, col_labels, meassurment_unit, x_figsize, y_
     fig, ax = plt.subplots(figsize=(x_figsize, y_figsize))
     cax = ax.imshow(matrix, aspect="auto")
 
-    cbar = fig.colorbar(cax)
+    fig.colorbar(cax)
 
     ax.set_xticks(np.arange(len(col_labels)))
     ax.set_xticklabels(col_labels)
